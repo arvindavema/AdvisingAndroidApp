cdb381d12437207a3f2d362d3376684fb9c6e783
Rohan <rkumar18@umd.edu>
2022-04-10 22:15:27 -0400

    Adding degree requirements

--- app/src/main/java/com/example/advisingv2/ui/DegreerequirementsFragment.kt
+++ app/src/main/java/com/example/advisingv2/ui/DegreerequirementsFragment.kt
@@ -1,11 +1,19 @@
 package com.example.advisingv2.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.Group
+import androidx.fragment.app.Fragment
 import com.example.advisingv2.R
+import com.example.advisingv2.databinding.FragmentDegreerequirementsBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,9 +27,13 @@ private const val ARG_PARAM2 = "param2"
  */
 class DegreerequirementsFragment : Fragment() {
 	// TODO: Rename and change types of parameters
+	private var _binding: FragmentDegreerequirementsBinding? = null
 	private var param1: String? = null
 	private var param2: String? = null
-	
+	private var cardView: CardView? = null
+	private var expand: Button? = null
+	private var hiddenGroup: TextView? = null
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		arguments?.let {
@@ -29,13 +41,77 @@ class DegreerequirementsFragment : Fragment() {
 			param2 = it.getString(ARG_PARAM2)
 		}
 	}
+
+	private val binding: FragmentDegreerequirementsBinding
+		get() = _binding!!
 	
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_degreerequirements, container, false)
+		_binding = FragmentDegreerequirementsBinding.inflate(inflater, container, false)
+		val view = binding.root
+		//cardView = binding.genTrackCard
+		//expand = binding.gtExpand
+		//hiddenGroup = binding.gtInfo
+
+		binding.gtExpand.setOnClickListener{
+			if (binding.gtInfo!!.visibility != View.VISIBLE) {
+				TransitionManager.beginDelayedTransition(binding.genTrackCard, AutoTransition())
+				binding.gtInfo!!.visibility = View.VISIBLE
+				binding.gtExpand.text = "Read Less"
+			} else {
+				TransitionManager.beginDelayedTransition(binding.genTrackCard, AutoTransition())
+				binding.gtInfo!!.visibility = View.GONE
+				binding.gtExpand.text = "Read More"
+			}
+		}
+		binding.cyberExpand.setOnClickListener{
+			if (binding.cyberInfo!!.visibility != View.VISIBLE) {
+				TransitionManager.beginDelayedTransition(binding.cyberCard, AutoTransition())
+				binding.cyberInfo!!.visibility = View.VISIBLE
+				binding.cyberExpand.text = "Read Less"
+			} else {
+				TransitionManager.beginDelayedTransition(binding.cyberCard, AutoTransition())
+				binding.cyberInfo!!.visibility = View.GONE
+				binding.cyberExpand.text = "Read More"
+			}
+		}
+		binding.dsExpand.setOnClickListener{
+			if (binding.dsInfo!!.visibility != View.VISIBLE) {
+				TransitionManager.beginDelayedTransition(binding.dsCard, AutoTransition())
+				binding.dsInfo!!.visibility = View.VISIBLE
+				binding.dsExpand.text = "Read Less"
+			} else {
+				TransitionManager.beginDelayedTransition(binding.dsCard, AutoTransition())
+				binding.dsInfo!!.visibility = View.GONE
+				binding.dsExpand.text = "Read More"
+			}
+		}
+		binding.qiExpand.setOnClickListener{
+			if (binding.qiInfo!!.visibility != View.VISIBLE) {
+				TransitionManager.beginDelayedTransition(binding.qiCard, AutoTransition())
+				binding.qiInfo!!.visibility = View.VISIBLE
+				binding.qiExpand.text = "Read Less"
+			} else {
+				TransitionManager.beginDelayedTransition(binding.qiCard, AutoTransition())
+				binding.qiInfo!!.visibility = View.GONE
+				binding.qiExpand.text = "Read More"
+			}
+		}
+		binding.mlExpand.setOnClickListener{
+			if (binding.mlInfo!!.visibility != View.VISIBLE) {
+				TransitionManager.beginDelayedTransition(binding.mlCard, AutoTransition())
+				binding.mlInfo!!.visibility = View.VISIBLE
+				binding.mlExpand.text = "Read Less"
+			} else {
+				TransitionManager.beginDelayedTransition(binding.mlCard, AutoTransition())
+				binding.mlInfo!!.visibility = View.GONE
+				binding.mlExpand.text = "Read More"
+			}
+		}
+		return view
 	}
 	
 	companion object {
