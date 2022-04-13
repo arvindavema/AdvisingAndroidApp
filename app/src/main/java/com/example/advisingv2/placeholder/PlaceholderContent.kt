41104b9f8d258852b2ebd5bddffd61c03230d840
Rohan <rkumar18@umd.edu>
2022-04-13 15:31:03 -0400

    course list

--- app/src/main/java/com/example/advisingv2/placeholder/PlaceholderContent.kt
+++ app/src/main/java/com/example/advisingv2/placeholder/PlaceholderContent.kt
@@ -21,8 +21,9 @@ object PlaceholderContent {
 	 */
 	val ITEM_MAP: MutableMap<String, PlaceholderItem> = HashMap()
 	
-	private val COUNT = 25
-	
+	private val COUNT = 78
+	private val courses = arrayOf("100", "122", "125", "131", "132", "133", "216", "250", "298A", "320", "330", "335", "351", "396H", "411", "412", "414", "416", "417", "420", "421", "422", "423", "424", "425", "426", "427", "430", "433", "434", "435", "436", "451", "454", "456","460", "466", "470", "473", "474", "475", "488A", "498A", "498I", "499A", "614", "624", "630", "631", "657", "660", "666", "673", "714", "715", "723", "730", "733", "734", "740", "751", "756", "798", "799", "801", "818X", "828C", "828J", "828V", "828W", "828X", "829A", "838X", "848B", "848C", "858F", "858O", "898", "899")
+
 	init {
 		// Add some sample items.
 		for (i in 1..COUNT) {
@@ -36,7 +37,7 @@ object PlaceholderContent {
 	}
 	
 	private fun createPlaceholderItem(position: Int): PlaceholderItem {
-		return PlaceholderItem(position.toString(), "Item " + position, makeDetails(position))
+		return PlaceholderItem(position.toString(), "CMSC" + courses[position], makeDetails(position))
 	}
 	
 	private fun makeDetails(position: Int): String {
